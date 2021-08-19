f7f6f295469d7bc207cad90fb86846cbd971f297
tomoya530 <sugimoto.tomoya530@gmail.com>
2021-08-19 23:25:24 +0900

    メソッド2

---
+++ src/practice/Car.java
@@ -0,0 +1,15 @@
+package practice;
+
+public class Car {
+	private double fuelCost;
+	// 残量（L）
+	private double fuelAmount;
+
+	// コンストラクタを作成
+
+	// moveメソッド
+	// ・"xx km 走ります"を出力
+	// ・残量を計算
+
+	// fuelAmountを取得するメソッドを作成
+}
